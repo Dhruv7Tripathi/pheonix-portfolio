024f4af3b9b85051e9db7e38953790faebe2b75a
Dhruv7Tripathi <tripathidhruv366@gmail.com>
2025-08-12 15:13:12 +0530

    refactor: reorganize Navbar component and update import paths

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,29 +2,49 @@
 import React from 'react';
 import Link from 'next/link';
 import { Menu, X } from 'lucide-react';
+import { Button } from './ui/button';
 import { cn } from '@/lib/utils';
-import { Themetoggle } from '../ui/themetoggle';
+import Image from 'next/image';
 
 const menuItems = [
   { name: 'Home', href: '/' },
-  { name: 'Projects', href: '#projects' },
-  { name: 'Blogs', href: '/blogs' },
-  { name: 'Social', href: '#social' },
+  { name: 'Posts', href: '/allpost' },
+  { name: 'write', href: '/write' },
 ]
 
 const Navbar = () => {
   const [menuState, setMenuState] = React.useState(false)
+  const [isScrolled, setIsScrolled] = React.useState(false)
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
   return (
     <header>
       <nav
         data-state={menuState && 'active'}
-        className="w-full px-2 group">
-        <div className={cn('mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12')}>
-          <div className="relative flex text-xl mr-24 items-center justify-between w-full py-3 lg:py-4">
-            <div className='text-6xl px-16 ml-16 font-bold'>
-              <Themetoggle />
-            </div>
-            <div className="flex w-full lg:w-auto justify-end">
+        className="fixed z-20 w-full px-2 group">
+        <div className={cn('mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12', isScrolled && 'bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5')}>
+          <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+            <div className="flex w-full justify-between lg:w-auto">
+              <Link
+                href="/"
+                aria-label="home"
+                className="flex items-center space-x-2">
+                <Image
+                  src="/logo.jpg"
+                  alt="Bloggify Logo"
+                  width={40}
+                  height={40}
+                  className="h-10 w-10 rounded-full object-cover lg:h-10 lg:w-10 md:h-10 md:w-10 sm:h-8 sm:w-8"
+                  priority
+                />
+              </Link>
+
               <button
                 onClick={() => setMenuState(!menuState)}
                 aria-label={menuState == true ? 'Close Menu' : 'Open Menu'}
@@ -34,13 +54,13 @@ const Navbar = () => {
               </button>
             </div>
 
-            <div className="hidden lg:block ml-auto">
+            <div className="absolute inset-0 m-auto hidden size-fit lg:block">
               <ul className="flex gap-8 text-sm">
                 {menuItems.map((item, index) => (
                   <li key={index}>
                     <Link
                       href={item.href}
-                      className="text-black text-xl  hover:text-accent-foreground dark:text-white block duration-150">
+                      className="text-black dark:text-white hover:text-accent-foreground block duration-150">
                       <span>{item.name}</span>
                     </Link>
                   </li>
@@ -49,18 +69,34 @@ const Navbar = () => {
             </div>
 
             <div className="bg-background group-data-[state=active]:block lg:group-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
-              <div className="lg:hidden justify-end">
+              <div className="lg:hidden">
                 <ul className="space-y-6 text-base">
                   {menuItems.map((item, index) => (
                     <li key={index}>
                       <Link
                         href={item.href}
-                        className="text-black justify-end dark:text-white hover:text-accent-foreground block duration-150">
+                        className="text-white hover:text-accent-foreground block duration-150">
                         <span>{item.name}</span>
                       </Link>
                     </li>
                   ))}
                 </ul>
+              </div>
+              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+                <Button
+                  asChild
+                  variant="outline"
+                  size="sm"
+                  className={cn(isScrolled && 'lg:hidden')}>
+
+                  <Link
+                    href="/api/auth/signin"
+                    className="text-white hover:text-accent-foreground block duration-150">
+                    <span>Sign In</span>
+                  </Link>
+
+
+                </Button>
               </div>
             </div>
           </div>
